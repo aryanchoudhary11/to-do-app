93e77d5a40303811226268a10a3853035909c687
Aryan <119430003+aryanchoudhary11@users.noreply.github.com>
2024-10-24 22:00:02 +0530

    CSS applied

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
-let todoList = [{item: '', date: ''}];
+let todoList = [];
 
 function addTodo() {
     let inputElement = document.querySelector('#todo-input');
     let dateElement = document.querySelector('#todo-date');
     let todotItem = inputElement.value;
     let datetItem = dateElement.value;
-    todoList.push({item: todotItem, date: datetItem});
-    inputElement.value = '';
-    dateElement.value = '';
+    if(todotItem === '' || datetItem === ''){
+        alert('Please, Give valid input!');
+    } else {
+
+        todoList.push({item: todotItem, date: datetItem});
+        inputElement.value = '';
+        dateElement.value = '';
+    }
 
 
     displayItems();
 }
 
 function displayItems(){
-    let containerElement = document.querySelector('#todo-container');
+    let containerElement = document.querySelector('.todo-container');
     let newHtml = '';
     for(let i = 0; i < todoList.length; i++){
+
         let {item, date} = todoList[i];
         newHtml+= `
-        <div>
+
             <span>${item}</span>
             <span>${date}</span>
-            <button onclick = "todoList.splice(${i},1);
+            <button class="delete-btn" onclick = "todoList.splice(${i},1);
             displayItems();
             ">Delete</button>
-        </div>
         `
     }
     containerElement.innerHTML = newHtml;
